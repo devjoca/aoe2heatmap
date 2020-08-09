54345eb5f4174158e63c8d3b6ef6d07078025ee3
devjoca <jpereyraleon@gmail.com>
2020-08-09 13:46:20 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 
-function App() {
+
+const groupBy = function (arr, criteria) {
+  return arr.reduce(function (obj, item) {
+    var key = item[criteria];
+    if (!obj.hasOwnProperty(key)) {
+      obj[key] = [];
+    }
+    obj[key].push(item);
+    return obj;
+  }, {});
+};
+
+const App = () => {
+  const mapContainerRef = useRef(null);
+  // const [data, setData] = useRef([])
+
+  useEffect(() => {
+    axios.get('https://thingproxy.freeboard.io/fetch/https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count=100')
+      .then(({ data }) => {
+        console.log(groupBy(data.leaderboard, 'country'))
+      })
+  });
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [-76.679, -12.045],
+      zoom: 1.5,
+    });
+
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    return () => map.remove();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="map-container" ref={mapContainerRef} />
   );
 }
 
