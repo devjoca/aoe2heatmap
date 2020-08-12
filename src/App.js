0b25f8d68484c04c8479c8346c8081d6c056b9b8
devjoca <jpereyraleon@gmail.com>
2020-08-11 22:36:33 -0500

    workable example mapbox layers

--- src/App.js
+++ src/App.js
@@ -1,43 +1,84 @@
 import React, { useRef, useEffect } from 'react';
-import './App.css';
+import { scaleQuantile } from "d3-scale";
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
+import useSWR from 'swr';
 
+import './App.css';
 
-const groupBy = function (arr, criteria) {
-  return arr.reduce(function (obj, item) {
-    var key = item[criteria];
+const summarizeData = function (arr) {
+  return arr.reduce(function (obj, profile) {
+    var key = profile['country'];
     if (!obj.hasOwnProperty(key)) {
-      obj[key] = [];
+      obj[key] = {};
+      obj[key]['players'] = [];
+      obj[key]['totalRatings'] = 0;
     }
-    obj[key].push(item);
+    obj[key]['players'].push(profile);
+    obj[key]['totalRatings'] += profile.rating;
     return obj;
   }, {});
 };
 
+const fetcher = url => {
+  return axios.get('https://thingproxy.freeboard.io/fetch/https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count=100')
+    .then(({ data }) => {
+      return summarizeData(data.leaderboard);
+    })
+}
+
 const App = () => {
   const mapContainerRef = useRef(null);
-  // const [data, setData] = useRef([])
+  const { data } = useSWR('https://thingproxy.freeboard.io/fetch/https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count=100', fetcher)
+
 
   useEffect(() => {
-    axios.get('https://thingproxy.freeboard.io/fetch/https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count=100')
-      .then(({ data }) => {
-        console.log(groupBy(data.leaderboard, 'country'))
-      })
-  });
+    if (data) {
+      const colorScale = scaleQuantile()
+        .domain(Object.keys(data).map(k => data[k].totalRatings / data[k].players.length))
+        .range([
+          "#ffedea",
+          "#ffcec5",
+          "#ffad9f",
+          "#ff8a75",
+          "#ff5533",
+          "#e2492d",
+          "#be3d26",
+          "#9a311f",
+          "#782618"
+        ]);
 
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/dark-v10',
-      center: [-76.679, -12.045],
-      zoom: 1.5,
-    });
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/dark-v10',
+        center: [-82.366592, 23.113592],
+        zoom: 2,
+      });
+      map.on('load', function () {
+        map.addLayer({
+          id: 'countries',
+          source: {
+            type: 'vector',
+            url: 'mapbox://jopereyral.3qrx3sp4',
+          },
+          'source-layer': 'ne_10m_admin_0_countries-3iszu5',
+          type: 'fill',
+          paint: {
+            "fill-opacity": 0.8,
+            'fill-color': colorScale(0),
+          },
+        });
+      });
 
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+      map.on('click', 'countries', (mapEl) => {
+        console.log(mapEl.features[0])
+      });
 
-    return () => map.remove();
-  }, []);
+      // map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+      return () => map.remove();
+    }
+
+  }, [data]);
 
   return (
     <div className="map-container" ref={mapContainerRef} />
